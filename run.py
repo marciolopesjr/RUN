d389bbd39226638681c83c9a3c50d32723f132e3
Márcio Lopes <marcioadrianolopesjunior@gmail.com>
2021-06-29 12:01:30 -0300

    nothing to say

--- run.py
+++ run.py
@@ -1,14 +1,22 @@
+import random 
 import pygame as pg 
 from pygame.math import Vector2
 
 def main():
     pg.init()
     screen = pg.display.set_mode((640, 480))
+    eye = pg.image.load('eye.png')
+    pg.display.set_icon(eye)
+    pg.display.set_caption("run!")
     clock = pg.time.Clock()
     image = pg.Surface((30, 30))
+    mirror = pg.image.load('mirror.png')
+    mX = random.randint(0, 640)
+    mY = random.randint(0, 480)
+    minMirror = pg.transform.scale(mirror, (48, 57))
     image.fill(pg.Color('dodgerblue1'))
     x, y = 300, 200 
-    rect = image.get_rect(center=(x, y))
+    rect = eye.get_rect(center=(x, y))
 
     while True:
         for event in pg.event.get():
@@ -23,10 +31,12 @@ def main():
         rect.center = x, y
 
         screen.fill((30, 30, 30))
-        screen.blit(image, rect)
+        screen.blit(eye, rect)
+        screen.blit(minMirror, (mX, mY))
 
         pg.display.flip()
         clock.tick(60)
+        print(clock)
 
 
 if __name__ == '__main__':
